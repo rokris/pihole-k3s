3c5971c0f9ca203377808a56ee6c70d3cf691929
Roger Kristiansen <rokris@hotmail.com>
2022-09-12 09:45:11 +0200

    Added files

---
+++ delete-pihole.sh
@@ -0,0 +1,4 @@
+kubectl delete -f 04-pihole-ingress.yaml
+kubectl delete -f 03-pihole-service.yaml
+kubectl delete -f 02-pihole-deployment.yaml
+kubectl delete -f 01-pihole-volume.yaml
---
+++ start-pihole.sh
@@ -0,0 +1,4 @@
+kubectl apply -f 01-pihole-volume.yaml
+kubectl apply -f 02-pihole-deployment.yaml
+kubectl apply -f 03-pihole-service.yaml
+kubectl apply -f 04-pihole-ingress.yaml
